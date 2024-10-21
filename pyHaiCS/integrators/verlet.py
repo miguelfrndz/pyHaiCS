90521b7c3ba8f360bf891eaee7d77fc4375aabe7
miguelfrndz <miguel.fretana@gmail.com>
2024-10-21 15:51:44 +0200

    Implemented HMC (Working)

--- pyHaiCS/integrators/verlet.py
+++ pyHaiCS/integrators/verlet.py
@@ -1,27 +1,30 @@
+import jax
 import jax.numpy as jnp
 from .integrator import Integrator
+from jax import tree_util
 
 class VerletIntegrator(Integrator):
+
     def __init__(self):
         pass
 
     def _position_full_step(self, x, p, step_size, mass_matrix):
         return x + step_size * jnp.linalg.solve(mass_matrix, p)
 
-    def _momentum_full_step(self, x, p, step_size, potential_grad):
-        return p - step_size * potential_grad(x)
+    def _momentum_full_step(self, x, p, step_size, potential_grad, potential_args):
+        return p - step_size * potential_grad(x, *potential_args)
 
-    def _momentum_half_step(self, x, p, step_size, potential_grad):
-        return p - step_size/2 * potential_grad(x)
-
-    def _verlet_step(self, x, p, step_size, potential_grad, mass_matrix):
+    def _momentum_half_step(self, x, p, step_size, potential_grad, potential_args):
+        return p - step_size/2 * potential_grad(x, *potential_args)
+    
+    def _verlet_step(self, x, p, step_size, potential_grad, potential_args, mass_matrix):
         # Full-Step update for position
         x = self._position_full_step(x, p, step_size, mass_matrix)
         # Full-Step update for momentum
-        p = self._momentum_full_step(x, p, step_size, potential_grad)
+        p = self._momentum_full_step(x, p, step_size, potential_grad, potential_args)
         return x, p
-
-    def integrate(self, x, p, potential_grad, n_steps, mass_matrix, step_size):
+    
+    def integrate(self, x, p, potential_grad, potential_args, n_steps, mass_matrix, step_size):
         """
         Verlet integration for Hamiltonian dynamics.
         -------------------------
@@ -29,7 +32,8 @@ class VerletIntegrator(Integrator):
         x (jax.Array): position
         p (jax.Array): momentum
         potential_grad (function): potential gradient
-        n_steps (int): number of steps
+        potential_args (tuple): arguments for potential gradient
+        n_steps (int): number of integration steps
         mass_matrix (jax.Array): mass matrix
         step_size (float): step size
         -------------------------
@@ -38,11 +42,11 @@ class VerletIntegrator(Integrator):
         p (jax.Array): updated momentum
         """
         # Half-Step update for momentum
-        p = self._momentum_half_step(x, p, step_size, potential_grad)
+        p = self._momentum_half_step(x, p, step_size, potential_grad, potential_args)
         for _ in range(n_steps - 1):
-            x, p = self._verlet_step(x, p, step_size, potential_grad, mass_matrix)
+            x, p = self._verlet_step(x, p, step_size, potential_grad, potential_args, mass_matrix)
         # Full-Step update for position
         x = self._position_full_step(x, p, step_size, mass_matrix)
         # Half-Step update for momentum
-        p = self._momentum_half_step(x, p, step_size, potential_grad)
+        p = self._momentum_half_step(x, p, step_size, potential_grad, potential_args)
         return x, p
--- pyHaiCS/samplers/hamiltonian.py
+++ pyHaiCS/samplers/hamiltonian.py
@@ -1,5 +1,76 @@
-def HMC(n_samples, step_size, n_steps, potential_grad, mass_matrix, integrator):
-    pass
+import jax
+import jax.numpy as jnp
+from tqdm import tqdm
+
+@jax.jit
+def Kinetic(p, mass_matrix):
+    """
+    Kinetic energy function.
+    -------------------------
+    Parameters:
+        p (jax.Array): momentum
+        mass_matrix (jax.Array): mass matrix
+    -------------------------
+    Returns:
+        K (float): kinetic energy
+    """
+    K = 0.5 * jnp.dot(p, jnp.linalg.solve(mass_matrix, p))
+    return K
+
+def Hamiltonian(x, p, potential, potential_args, mass_matrix):
+    """
+    Hamiltonian function.
+    -------------------------
+    Parameters:
+        x (jax.Array): position
+        p (jax.Array): momentum
+        potential_grad (function): potential gradient
+        mass_matrix (jax.Array): mass matrix
+    -------------------------
+    Returns:
+        H (float): Hamiltonian
+    """
+    K = Kinetic(p, mass_matrix)
+    U = potential(x, *potential_args)
+    H = K + U
+    return H
+
+def HMC(x_init, potential_args, n_samples, burn_in, step_size, n_steps, potential, mass_matrix, integrator, n_chains = 4, RNG_key = 42):
+    """
+    Multi-Chain Hamiltonian Monte Carlo sampler.
+    -------------------------
+    Parameters:
+        n_samples (int): number of samples
+        burn_in (int): burn-in samples
+        step_size (float): step size
+        n_steps (int): number of integration steps
+        potential (function): Hamiltonian potential
+        mass_matrix (jax.Array): mass matrix
+        integrator (object): integrator object
+    -------------------------
+    Returns:
+        samples (jax.Array): samples
+    """
+    print("Running HMC sampler...")
+    key = jax.random.PRNGKey(RNG_key)
+    potential = jax.jit(potential)
+    potential_grad = jax.grad(potential)
+    samples = []
+    x = x_init
+    for n in tqdm(range(n_samples + burn_in)):
+        # Initial momentum (gaussian), shape given by mass matrix
+        p = jax.random.multivariate_normal(key, jnp.zeros(x.shape[0]), mass_matrix)
+        # Integrate Hamiltonian dynamics
+        x_prop, p_prop = integrator.integrate(x, p, potential_grad, potential_args, n_steps, mass_matrix, step_size)
+        # Computer enery error
+        delta_H = Hamiltonian(x_prop, p_prop, potential, potential_args, mass_matrix) - Hamiltonian(x, p, potential, potential_args, mass_matrix)
+        # Metropolis-Hastings acceptance
+        if jax.random.uniform(jax.random.PRNGKey(0)) < jnp.exp(-delta_H):
+            x = x_prop
+        if n >= burn_in:
+            samples.append(x)
+    samples = jnp.stack(samples, axis=0)
+    return samples
 
 def GHMC():
     pass
