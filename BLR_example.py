542c2985c3060d5d77dfadda02dd75ba465fa9f3
miguelfrndz <miguel.fretana@gmail.com>
2024-10-22 13:11:26 +0200

    Implemented GHMC

--- BLR_example.py
+++ BLR_example.py
@@ -92,4 +92,32 @@ mean_preds = mean_preds > 0.5
 
 # Evaluate the model
 accuracy = jnp.mean(mean_preds == y_test)
-print(f"Accuracy: {accuracy}")
+print(f"Accuracy (w/ HMC Sampling): {accuracy}\n")
+
+########################### GHMC ###########################
+
+# Momentum noise is randomly chosen between 0 and 1 (0 not included)
+momentum_noise = jax.random.uniform(key, minval = 0.01, maxval = 1.0)
+
+# GHMC for posterior sampling
+params_samples = haics.samplers.hamiltonian.GHMC(params, 
+                            potential_args = (X_train, y_train),                                           
+                            n_samples = 1000, burn_in = 200, 
+                            step_size = 1e-3, n_steps = 100, 
+                            potential = neg_log_posterior_fn,  
+                            mass_matrix = jnp.eye(X_train.shape[1]), 
+                            momentum_noise = momentum_noise,
+                            integrator = haics.integrators.VerletIntegrator(), 
+                            RNG_key = 120)
+
+# Average across chains
+params_samples = jnp.mean(params_samples, axis = 0)
+
+# Make predictions using the samples
+preds = jax.vmap(lambda params: model_fn(params, X_test))(params_samples)
+mean_preds = jnp.mean(preds, axis=0)
+mean_preds = mean_preds > 0.5
+
+# Evaluate the model
+accuracy = jnp.mean(mean_preds == y_test)
+print(f"Accuracy (w/ GHMC Sampling): {accuracy}\n")
--- pyHaiCS/samplers/hamiltonian.py
+++ pyHaiCS/samplers/hamiltonian.py
@@ -38,7 +38,7 @@ def Hamiltonian(x, p, potential, mass_matrix):
 def _single_chain_HMC(x_init, n_samples, burn_in, step_size, n_steps, 
         potential, potential_grad, mass_matrix, integrator, key):
     """
-    Single-Chain Hamiltonian Monte Carlo sampler.
+    Single-Chain Hamiltonian Monte-Carlo (HMC) sampler.
     -------------------------
     Parameters:
         n_samples (int): number of samples
@@ -72,14 +72,16 @@ def _single_chain_HMC(x_init, n_samples, burn_in, step_size, n_steps,
     return samples
 
 def HMC(x_init, potential_args, n_samples, burn_in, step_size, n_steps, 
-        potential, mass_matrix, integrator, n_chains=4, RNG_key=42):
+        potential, mass_matrix, integrator, n_chains = 4, RNG_key = 42):
     """
-    Multi-Chain Hamiltonian Monte Carlo sampler.
+    Multi-Chain Hamiltonian Monte-Carlo (HMC) sampler.
     -------------------------
     Parameters:
+        x_init (jax.Array): initial position
+        potential_args (tuple): arguments for Hamiltonian potential
         n_samples (int): number of samples
         burn_in (int): burn-in samples
-        step_size (float): step size
+        step_size (float): step-size
         n_steps (int): number of integration steps
         potential (function): Hamiltonian potential
         mass_matrix (jax.Array): mass matrix
@@ -106,8 +108,86 @@ def HMC(x_init, potential_args, n_samples, burn_in, step_size, n_steps,
     samples = vectorized_chain(x_init_repeated, n_samples, burn_in, step_size, n_steps, potential, potential_grad, mass_matrix, integrator, keys)
     return samples
 
-def GHMC():
-    pass
+def _single_chain_GHMC(x_init, n_samples, burn_in, step_size, n_steps, 
+        potential, potential_grad, mass_matrix, momentum_noise, integrator, key):
+    """
+    Single-Chain Generalized Hamiltonian Monte-Carlo (GHMC) sampler.
+    -------------------------
+    Parameters:
+        n_samples (int): number of samples
+        burn_in (int): burn-in samples
+        step_size (float): step size
+        n_steps (int): number of integration steps
+        potential (function): Hamiltonian potential
+        potential_grad (function): Hamiltonian potential gradient
+        mass_matrix (jax.Array): mass matrix
+        momentum_noise (float): momentum noise
+        integrator (object): integrator object
+    -------------------------
+    Returns:
+        samples (jax.Array): samples
+    """
+    samples = []
+    x = x_init
+    # Initial momentum (gaussian), shape given by mass matrix
+    p = jax.random.multivariate_normal(key, jnp.zeros(x.shape[0]), mass_matrix)
+    for n in tqdm(range(n_samples + burn_in)):
+        key, subkey = jax.random.split(key)
+        # Sample noise vector
+        mu = jax.random.multivariate_normal(subkey, jnp.zeros(x.shape[0]), mass_matrix)
+        # Propose updated momentum and noise vector
+        p_prop = jnp.sqrt(1 - momentum_noise) * p + jnp.sqrt(momentum_noise) * mu
+        mu_prop = -jnp.sqrt(momentum_noise) * p + jnp.sqrt(1 - momentum_noise) * mu
+        # Integrate Hamiltonian dynamics
+        x_new, p_new = integrator.integrate(x, p_prop, potential_grad, n_steps, mass_matrix, step_size)
+        # Computer enery error
+        delta_H = Hamiltonian(x_new, p_new, potential, mass_matrix) - Hamiltonian(x, p_prop, potential, mass_matrix)
+        # Metropolis-Hastings acceptance
+        accept = jax.random.uniform(subkey) < jnp.exp(-delta_H)
+        x, p = jax.lax.cond(accept, lambda _:(x_new, p_new), lambda _:(x, -p_prop), operand=None)
+        if n >= burn_in:
+            samples.append(x)
+    samples = jnp.stack(samples, axis = 0)
+    return samples
+
+def GHMC(x_init, potential_args, n_samples, burn_in, step_size, n_steps, 
+        potential, mass_matrix, momentum_noise, integrator, n_chains = 4, RNG_key = 42):
+    """
+    Multi-Chain Generalized Hamiltonian Monte-Carlo (GHMC) sampler.
+    -------------------------
+    Parameters:
+        x_init (jax.Array): initial position
+        potential_args (tuple): arguments for Hamiltonian potential
+        n_samples (int): number of samples
+        burn_in (int): burn-in samples
+        step_size (float): step-size
+        n_steps (int): number of integration steps
+        potential (function): Hamiltonian potential
+        mass_matrix (jax.Array): mass matrix
+        momentum_noise (float): momentum noise
+        integrator (object): integrator object
+        n_chains (int): number of chains
+        RNG_key (int): random number generator key
+    -------------------------
+    Returns:
+        samples (jax.Array): samples
+    """
+    print("Running GHMC sampler...")
+    print("="*61)
+    print(f"{'Num. Chains':^30}|{n_chains:^30}")
+    print(f"{'Num. Samples':^30}|{n_samples:^30}")
+    print(f"{'Num. Burn-In Iterations':^30}|{burn_in:^30}")
+    print(f"{'Step-Size':^30}|{step_size:^30}")
+    print(f"{'Num. Integration Steps':^30}|{n_steps:^30}")
+    print(f"{'Momentum Noise':^30}|{momentum_noise:^30}")
+    print("="*61)
+    keys = jax.random.split(jax.random.PRNGKey(RNG_key), n_chains)
+    x_init_repeated = jnp.repeat(x_init[None, :], n_chains, axis = 0)
+    potential = jax.tree_util.Partial(potential, *potential_args)
+    potential_grad = jax.grad(potential)
+    vectorized_chain = jax.vmap(_single_chain_GHMC, in_axes=(0, None, None, None, None, None, None, None, None, None, 0))
+    samples = vectorized_chain(x_init_repeated, n_samples, burn_in, step_size, n_steps, potential, potential_grad, mass_matrix, momentum_noise, integrator, keys)
+    return samples
 
 def MMHMC():
     pass
