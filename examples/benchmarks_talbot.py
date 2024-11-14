e26753c19e057cfb93c9bfb7b1bd5eedaa96f55c
miguelfrndz <miguel.fretana@gmail.com>
2024-11-14 21:02:22 +0100

    Refactored Talbot Code

---
+++ examples/benchmarks_talbot.py
@@ -0,0 +1,72 @@
+import jax
+import os, sys
+import numpy as np
+from tqdm import tqdm
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+from talbot_utils import generate_amplitude_field, resize_field, plot_field
+
+class TalbotConfig:
+    def __init__(self):
+        self.A = 1 # Amplitude of signal
+        self.c = 1 # Speed of light
+        self.d = 1 # Distance between gratings we fix it = 1
+        self._lambda = self.d / 10 # Wavelength
+        self.w = 2 * self._lambda # Width of the gratings
+
+        # Other relevant magnitudes
+        self.omega = 2 * jnp.pi * self.c / self._lambda # Frequency of the signal
+        self.z_T = self._lambda/(1 - np.sqrt(1-(self._lambda/self.d) ** 2)) # Talbot distance = 2 d^2/λ
+
+        # Simulation parameters
+        self.N_x = 27 # Number of samples in x direction
+        self.N_z = 192 # Number of samples in z direction
+        self.N_t = 40 # Number of samples in time
+        self.N_max = int(self.d / self._lambda * 10) # Number of terms in the series
+
+        # Other relevant magnitudes
+        self.delta_t = self.z_T/self.c/self.N_t # Time between photos
+        self.delta_x = self.d/2/self.N_x # X-Distance between points
+        self.delta_z = self.z_T/self.N_z # Z-Distance between points
+
+    def __str__(self):
+        params = {
+            "Amplitude of signal (A)": self.A,
+            "Speed of light (c)": self.c,
+            "Distance between gratings (d)": self.d,
+            "Wavelength (lambda)": self._lambda,
+            "Width of the gratings (w)": self.w,
+            "Frequency of the signal (omega)": self.omega,
+            "Talbot distance (z_T)": self.z_T,
+            "Number of samples in x direction (N_x)": self.N_x,
+            "Number of samples in z direction (N_z)": self.N_z,
+            "Number of samples in time (N_t)": self.N_t,
+            "Number of terms in the series (N_max)": self.N_max,
+            "Time between photos (delta_t)": self.delta_t,
+            "X-Distance between points (delta_x)": self.delta_x,
+            "Z-Distance between points (delta_z)": self.delta_z,
+        }
+        
+        print("{:<45} {:<40}".format('Parameter', 'Value'))
+        print("-" * 65)
+        for key, value in params.items():
+            print("{:<45} {:<40}".format(key, value))
+        return ""
+
+if __name__ == "__main__":
+    config = TalbotConfig()
+    print(config)
+
+    # Photo destination
+    my_path = os.path.dirname(os.path.abspath(__file__))
+    folder_name = 'd_λ=' + str(config.d/config._lambda) + '_w_λ=' + str(config.w/config._lambda)
+    folder_path = os.path.join(my_path, folder_name)
+
+    # Create the folder if it doesn't exist
+    if not os.path.isdir(folder_path):
+        os.makedirs(folder_path)
+
+    field = generate_amplitude_field(config)
+    field = resize_field(field, config)
+    for t_i in tqdm(range(0, config.N_t)):
+        plot_field(t_i, field, config, folder_path)
---
+++ examples/talbot_utils.py
@@ -0,0 +1,95 @@
+import jax
+import os, sys
+import numpy as np
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+from functools import partial
+
+def integrate(t, z, k_n, config):
+    return jnp.sin(k_n * (config.c * t - z)) / (k_n * config.c)
+
+def h_n_vectorised(n, config):
+    return jax.lax.cond(n == 0, lambda _: 2 * config.w / config.d,
+            lambda _: 2 * jnp.sin(n * jnp.pi * config.w / config.d) / (jnp.pi * n),
+            operand = None
+            )
+
+def coefficient_ntz(n, t, z, config):
+    k_n = 2 * jnp.pi * n / config.d
+    c_n = k_n * integrate(t, z, k_n, config)
+    c_n += jnp.sin(config.omega * (t + z / config.c))*(1 - jnp.sin(k_n * (config.c * t + z))) * jnp.heaviside(t + z / config.c , 0.5)
+    c_n += jnp.sin(config.omega * (t - z / config.c))*(1 - jnp.sin(k_n * (config.c * t - z))) * jnp.heaviside(t - z / config.c , 0.5)
+    c_n *= config.A * h_n_vectorised(n, config) / 2
+    return c_n
+
+def generate_coeffs(config):
+    # We create an array to store the coordinates of the grid
+    coords = jnp.indices((config.N_max, config.N_t, config.N_z))
+    
+    # We normalise the array elements
+    coords = coords.astype(jnp.float64)
+    coords = coords.at[1,:,:,:].multiply(config.delta_t)
+    coords = coords.at[2,:,:,:].multiply(config.delta_z)
+
+    # We reshape it so it is 2-dimensional now
+    coords = coords.reshape((3,-1)).T
+    
+    # We compute the coefficient for each n, t, z
+    coeffs = jax.vmap(coefficient_ntz)(*coords.T, config)
+
+    # We reshape coeffs back to the original grid shape
+    coeffs = coeffs.reshape((config.N_max, config.N_t, config.N_z))
+
+    return coeffs
+
+def generate_amplitude_field(config):
+    coeffs = generate_coeffs(config)
+    field = jnp.zeros([config.N_t, config.N_x, config.N_z])
+    k_n = 2 * jnp.pi * jnp.arange(config.N_max) / config.d
+    # Create the grid for the cosine terms
+    x_grid = jnp.arange(config.N_x) * config.delta_x
+    cos_values = jnp.cos(jnp.outer(k_n, x_grid))
+    del x_grid
+    # Compute the contribution to E from the cosine term. 
+    # For this we expand dimensions of coeffs and cos_values to match the desired broadcast shape.
+    # New shapes:       (N_max, N_t, 1, N_z)        (N_max, 1, N_x,1)
+    field_update = coeffs[:, :, jnp.newaxis, :] * cos_values[:, jnp.newaxis, :, jnp.newaxis]
+    # Sum over n (along axis 0) to update E
+    field += jnp.sum(field_update, axis = 0)  # Shape: (N_t, N_x, N_z)
+    return field
+
+def resize_field(field, config):
+    resized_field = np.empty([config.N_t, 4 * config.N_x, config.N_z])
+    reversed_order = np.array([n for n in range(config.N_x - 1, -1, -1)])
+    resized_field[:, 0:config.N_x, :] = field[:,0:config.N_x,:]
+    resized_field[:,config.N_x:2*config.N_x,:] = field[:,reversed_order, :]
+    resized_field[:,2*config.N_x:3*config.N_x,:] = field[:,0:config.N_x,:]
+    resized_field[:,3*config.N_x:4*config.N_x,:] = field[:,reversed_order, :]
+    return resized_field
+
+def plot_field(t_i, field, config, folder_path):
+    cm = 1/2.54
+    plt.figure(figsize=(32*cm, 18*cm))
+    plt.title('Gauge Field at $t = ' + str(round(t_i * config.delta_t/(config.z_T/config.c),4)) + '\\, Z_T/c$ for $\\frac{d}{\\lambda}='+str(config.d/config._lambda)+'$ and $\\frac{w}{\\lambda}=' + str(config.w/config._lambda)+'$', fontsize = 20, y = 1.05)
+    # Plot the Gauge Field
+    im = plt.imshow(field[t_i], cmap='RdBu_r', vmin=-config.A, vmax=config.A, interpolation='none')
+    # Label the X axis and set the ticks
+    plt.ylabel('Grating', fontsize = 18)
+    plt.ylim(0, 4 * config.N_x)
+    ticks_x = [0, config.N_x-1, 2 * config.N_x - 1, 3 * config.N_x - 1, 4 * config.N_x - 1]
+    labels_x = ['$-d$', '$-\\dfrac{d}{2}$', '$0$', '$\\dfrac{d}{2}$', '$d$']
+    plt.yticks(ticks_x, labels_x, fontsize = 16)
+    # Label the Y axis and set the ticks
+    plt.xlabel('Propagation of light ---->', fontsize = 18)
+    plt.xlim(0, config.N_z - 1)
+    ticks_z = [0, config.N_z/4, config.N_z/2, 3 * config.N_z/4, config.N_z]
+    labels_z = ['$0$', '$\\dfrac{1}{4} Z_T$', '$\\dfrac{1}{2} Z_T$', '$\\dfrac{3}{4} Z_T$', '$Z_T$']
+    plt.xticks(ticks_z, labels_z, fontsize = 16)
+    # Add the colorbar
+    cbar = plt.colorbar(im, ticks=[-config.A, -config.A/2, 0., config.A/2, config.A], fraction = 0.0458 * config.N_z/(4 * config.N_x), pad = 0.04, shrink = 0.9)
+    cbar.set_label(label = 'Amplitude of the field', fontsize = 18)
+    cbar.ax.set_yticklabels(['$-A$', '$-\\dfrac{A}{2}$', '$0$', '$\\dfrac{A}{2}$', '$A$'], fontsize = 16)
+
+    file_name = 'd_λ=' + str(config.d/config._lambda) + '_w_λ=' + str(config.w/config._lambda)+'_' + str(t_i) + '_carpet.pdf'
+    plt.savefig(os.path.join(folder_path, file_name), bbox_inches='tight')  
+    plt.close()
