c6c90d2a4a4c37e73a392739ce2731a41643884b
miguelfrndz <miguel.fretana@gmail.com>
2025-01-29 13:13:56 +0100

    Updated Talbot Code

--- examples/benchmarks_talbot.py
+++ examples/benchmarks_talbot.py
@@ -8,35 +8,71 @@ from talbot_utils import generate_amplitude_field, resize_field, plot_field
 
 class TalbotConfig:
     def __init__(self):
-        self.A = 1 # Amplitude of signal
+        """
+        Parameters of the simulation
+        -------------------------
+        A : Amplitude of the source.
+        c : Speed of light. Fixed at 1.
+        d : Distance between the gratings. We fix it = 1.
+        _lambda : Wavelength λ of the source.
+        w : Width of the gratings.
+
+        Related relevant magnitudes
+        ---------------------------
+        omega : Angular frequency of the source = 2∏C/λ.
+        z_T : Talbot distance = λ/(1 - √(1 - λ/d^2)).
+
+        Grid parameters
+        ---------------
+        N_t : Number of samples in the t direction.
+        N_x : Number of samples in the x direction.
+        N_z : Number of samples in the z direction.
+
+        Simulation parameters
+        ---------------------
+        N_max : Value of the series' cutoff. We want N_max >> d/λ.
+        initial_t_zT : Initial time / Z_t.
+        final_t_zT : Final time / Z_t.
+
+        Other relevant magnitudes
+        -------------------------
+        delta_t : Time between points = self.z_T/self.c/(self.N_t-1) * (self.final_t_zT - self.initial_t_zT).
+        delta_x : X-Distance between points = self.d/2/self.N_x.
+        delta_z : Z-Distance between points = self.z_T/self.N_z.
+        """
+        self.A = 1 # Amplitude of source
         self.c = 1 # Speed of light
         self.d = 1 # Distance between gratings we fix it = 1
         self._lambda = self.d / 10 # Wavelength
         self.w = 2 * self._lambda # Width of the gratings
 
-        # Other relevant magnitudes
-        self.omega = 2 * jnp.pi * self.c / self._lambda # Frequency of the signal
+        # Other related relevant magnitudes
+        self.omega = 2 * jnp.pi * self.c / self._lambda # Angular frequency of the source
         self.z_T = self._lambda/(1 - np.sqrt(1-(self._lambda/self.d) ** 2)) # Talbot distance = 2 d^2/λ
 
-        # Simulation parameters
-        self.N_x = 27 # Number of samples in x direction
-        self.N_z = 192 # Number of samples in z direction
-        self.N_t = 40 # Number of samples in time
-        self.N_max = int(self.d / self._lambda * 10) # Number of terms in the series
+        # Grid Simulation parameters
+        self.N_x = 27 * 10 # Number of samples in x direction
+        self.N_z = 192 * 10 # Number of samples in z direction
+        self.N_t = 250 # Number of samples in time
+
+        # Simulation parameters
+        self.N_max = int(self.d / self._lambda * 5) # Number of terms in the series
+        self.initial_t_zT = 0 # Initial time / Z_t
+        self.final_t_zT = 2 # Final time / Z_t
 
         # Other relevant magnitudes
-        self.delta_t = self.z_T/self.c/self.N_t # Time between photos
+        self.delta_t = self.z_T/self.c/(self.N_t - 1) * (self.final_t_zT - self.initial_t_zT) # Time between photos
         self.delta_x = self.d/2/self.N_x # X-Distance between points
         self.delta_z = self.z_T/self.N_z # Z-Distance between points
 
     def __str__(self):
         params = {
-            "Amplitude of signal (A)": self.A,
+            "Amplitude of the source (A)": self.A,
             "Speed of light (c)": self.c,
             "Distance between gratings (d)": self.d,
             "Wavelength (lambda)": self._lambda,
             "Width of the gratings (w)": self.w,
-            "Frequency of the signal (omega)": self.omega,
+            "Angular frequency of the source": self.omega,
             "Talbot distance (z_T)": self.z_T,
             "Number of samples in x direction (N_x)": self.N_x,
             "Number of samples in z direction (N_z)": self.N_z,
@@ -45,6 +81,8 @@ class TalbotConfig:
             "Time between photos (delta_t)": self.delta_t,
             "X-Distance between points (delta_x)": self.delta_x,
             "Z-Distance between points (delta_z)": self.delta_z,
+            "Initial time / z_T": self.initial_t_zT,
+            "Final time / z_T": self.final_t_zT,
         }
         
         print("{:<45} {:<40}".format('\nParameter', 'Value'))
--- examples/talbot_utils.py
+++ examples/talbot_utils.py
@@ -2,6 +2,7 @@ import jax
 import os, sys
 sys.path.append('../')
 import numpy as np
+from tqdm import tqdm
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 from functools import partial
@@ -10,120 +11,336 @@ jax.config.update("jax_enable_x64", True)
 from BesselJAX import J0, J1
 import pyHaiCS as haics
 
-# def integrate(t, z, k_n, config):
-#     @jax.jit
-#     def func(x):
-#         return J0(k_n * jnp.sqrt((config.c * jnp.tan(x)) ** 2 - z ** 2)) * jnp.sin(config.omega * (jnp.tan(x) + t))
-#     return quadgk(lambda x: func(x)/(jnp.cos(x) ** 2), [jnp.arctan(jnp.abs(z)/config.c), jnp.pi/2])[0]
-#     # return quadgk(lambda x: func(x), [jnp.abs(z)/config.c, jnp.inf])[0]
+def g_n_rect_delta(n, config):
+    '''Computes the g_n of the Rect function NORMALISED so that it tends to the delta function.
+    For n =/= 0 we return g_n + g_{-n} = 2 g_n
+    
+    Parameters
+    ----------
+    n : np.ndarray of ints
+        The ns for which to cumpute g_n. Must be integers.
 
-def integrate(t, z, k_n, config):
-    @jax.jit
-    def func(tau):
-        return jnp.where((tau - z  <= 1e-5), 
-                         k_n/2 * jnp.sin(config.omega * (tau - t)), 
-                         J1(k_n * jnp.sqrt(tau ** 2 - z ** 2))/jnp.sqrt(tau ** 2 - z ** 2) * jnp.sin(config.omega * (tau - t)))
-    # return quadcc(func, [z, t], max_ninter = 100)[0]
-    return quadgk(func, [z, t])[0]
+    config : TalbotConfig
+        The class containing all the parameters of the simulation.
+    
+    Returns
+    -------
+    g_n : np.ndarray of floats
+        The values of the g_ns relative to each n.
+    '''
 
-# def h_n_vectorised(n, config):
-#     return jax.lax.cond(n == 0, lambda _: 2 * config.w / config.d,
-#             lambda _: 2 * jnp.sin(n * jnp.pi * config.w / config.d) / (jnp.pi * n),
-#             operand = None
-#         )
+    original_settings = np.seterr()
+    np.seterr(divide='ignore', invalid='ignore')
+    result = np.where(n == 0, 1, 2*np.sin(n * np.pi * config.w) / (np.pi * n * config.w**2)) # We multiply by 2 to account at the same time for g_n and g_(-n)
+    np.seterr(**original_settings)
+    return result
 
-# def coefficient_ntz(n, t, z, config):
-#     k_n = 2 * jnp.pi * n / config.d
-#     c_n = k_n * integrate(t, z, k_n, config)
-#     c_n += jnp.sin(config.omega * (t + z / config.c))*(1 - jnp.sin(k_n * (config.c * t + z))) * jnp.heaviside(t + z / config.c , 0.5)
-#     c_n += jnp.sin(config.omega * (t - z / config.c))*(1 - jnp.sin(k_n * (config.c * t - z))) * jnp.heaviside(t - z / config.c , 0.5)
-#     c_n *= config.A * h_n_vectorised(n, config) / 2
-#     return c_n
+def perform_integrals(config):
+    '''This mehtod computes the integrals in (4.8) using GSL's integration method.
+    In order to minimise the size of the integrals, we only compute integrals in the intervals [t_i-1, t_i].
+    To recover the original interval [z_i, t_i] we only have to use Barrow's rule.
 
-def g_n_vectorised(n, config):
-    return jnp.where((n == 0), 2 * config.w, 2 * jnp.sin(n * jnp.pi * config.w) / (jnp.pi * n))
+    Note that we must have integrands independent of t, so we use the identity 
+    sin(tau-t) = cos(t)sin(tau) - sin(t)cos(tau) and separate each integral into two different ones.
 
-def coefficient_ntz(n, t, z, config):
-    k_n = 2 * jnp.pi * n / config.d
-    c_n = jnp.where((t <= z), 0.0, - k_n * z * integrate(t, z, k_n, config))
-    c_n += jnp.sin(config.omega * (t - z)) * jnp.heaviside(t - z / config.c , 0.5)
-    c_n *= g_n_vectorised(n, config)
-    return c_n
+    Credits to Jérôme Richard, who explained in his stackoverflow's answer stackoverflow.com/a/79360271/24208929 
+    how to call the GSL library from Python for the efficient evaluation of integrals.
+
+    Parameters
+    ----------
+    config : TalbotConfig
+        The class containing all the parameters of the simulation.
+
+    Returns
+    -------
+    result : np.ndarray of floats
+        The values of the integrals at (n,t_i,z_i). The array has shape (N_max, N_t, N_z).
+    '''
+
+    # We store the range of n, kn and gn in arrays of length N_max
+    n_values = np.linspace(0, config.N_max-1, config.N_max, dtype=int)
+    k_n_values = 2 * np.pi * n_values
+
+    # We store the range of t, z in the arrays of dimension (N_t) and (N_z)
+    t_values = np.linspace(config.z_T/config.c * config.initial_t_zT, config.z_T/config.c * config.final_t_zT, config.N_t)
+    z_values = np.linspace(0, config.N_z*config.delta_z, config.N_z)
+
+    # Preallocate the result arrays (shape: len(n_values) x len(z_values))
+    x_min = np.zeros((len(t_values), len(z_values)))
+    x_max = np.empty((len(t_values), len(z_values)))
+
+    # Compute the values
+    t1_values = np.roll(t_values, 1)
+    t1_values[0] = 0.
+    x_min = np.maximum(z_values[None, :], t1_values[:, None])  # Max between z_values[j] and t_values[i-1]
+    x_max = np.maximum(z_values[None, :], t_values[:, None])  # Max between z_values[j] and t_values[i]
+        
+    # Prealocate the result arrays
+    partial_integral_sin = np.zeros((len(n_values),len(t_values),len(z_values)))
+    partial_integral_cos = np.zeros((len(n_values),len(t_values),len(z_values)))
+
+
+    # Load the compiled libraries with ctypes
+    int_t = ctypes.c_int
+    double_t = ctypes.c_double
+    ptr_t = ctypes.POINTER(double_t)
+
+    # Utility function
+    ptr = lambda array: array.ctypes.data_as(ptr_t)
+
+    # We load the compiled libraries with ctypes
+    lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lib')
+    lib_extension = '.dylib' if os.name == 'posix' else '.so' # Depends on OS. dylib for Mac, so for UNIX
+    fast_integrals = ctypes.CDLL(os.path.join(lib_path, f'libintegrals{lib_extension}'))
+    fast_integrals.compute_cos.argtypes = [
+        ptr_t, ptr_t, ptr_t, ptr_t, ptr_t, ptr_t,
+        int_t, int_t, int_t, int_t,
+        double_t, double_t, double_t, double_t
+    ]
+    fast_integrals.compute_cos.restype = None
+
+    fast_integrals.compute_sin.argtypes = [
+        ptr_t, ptr_t, ptr_t, ptr_t, ptr_t, ptr_t,
+        int_t, int_t, int_t, int_t,
+        double_t, double_t, double_t, double_t
+    ]
+    fast_integrals.compute_sin.restype = None
+
+    # We perform the integrals
+    limit = 100_000
+    eps_abs = 1e-6
+    eps_rel = 1e-3
+    epsilon = 1e-6
+
+    fast_integrals.compute_cos(
+        ptr(partial_integral_cos), ptr(x_min), ptr(x_max), 
+        ptr(k_n_values), ptr(t_values), ptr(z_values), 
+        len(n_values), len(t_values), len(z_values), 
+        limit, eps_abs, eps_rel, 
+        config.omega, epsilon
+    )
+
+    fast_integrals.compute_sin(
+        ptr(partial_integral_sin), ptr(x_min), ptr(x_max), 
+        ptr(k_n_values), ptr(t_values), ptr(z_values), 
+        len(n_values), len(t_values), len(z_values), 
+        limit, eps_abs, eps_rel, 
+        config.omega, epsilon
+    )
+
+    # Initialize the result array
+    resummed_integral_sin = np.empty((len(n_values), len(t_values), len(z_values)))
+    resummed_integral_cos = np.empty((len(n_values), len(t_values), len(z_values)))
+
+    # Vectorized cumulative sum along the 't' axis (axis=1) to recover the full integrals
+    resummed_integral_sin[:, :, :] = np.cumsum(partial_integral_sin[:, :, :], axis=1)
+    resummed_integral_cos[:, :, :] = np.cumsum(partial_integral_cos[:, :, :], axis=1)
+
+    # Initialize the result array
+    result = np.empty((len(n_values), len(t_values), len(z_values)))
+    result = np.sin(config.omega * t_values[None,:,None]) * resummed_integral_cos \
+        - np.cos(config.omega * t_values[None,:,None]) * resummed_integral_sin # sin(tau-t) = cos(t)sin(tau) - sin(t)cos(tau)
+
+    return result
 
 def generate_coeffs(config):
-    # We create an array to store the coordinates of the grid
-    coords = jnp.indices((config.N_max, config.N_t, config.N_z))
+    '''Computes the coefficients $c_n(t,z)$ for each allowed value of n,t,z for the parameters in config.
     
-    # We normalise the array elements
-    coords = coords.astype(jnp.float64)
-    coords = coords.at[1,:,:,:].multiply(config.delta_t)
-    coords = coords.at[2,:,:,:].multiply(config.delta_z)
-
-    # We reshape it so it is 2-dimensional now
-    coords = coords.reshape((3,-1)).T
+    Parameters
+    ----------
+    config : TalbotConfig
+        The class containing all the parameters of the simulation.
     
-    # We compute the coefficient for each n, t, z
-    coeffs = jax.vmap(partial(coefficient_ntz, config = config))(*coords.T)
+    Returns
+    -------
+    coeffs : np.ndarray of floats
+        The values of the coefficients $c_n(t,z)$. The array is of shape (N_max, N_t, N_z).
+    '''
 
-    # We reshape coeffs back to the original grid shape
-    coeffs = coeffs.reshape((config.N_max, config.N_t, config.N_z))
+    # We store the range of t, z in the arrays of dimension (N_t) and (N_z)
+    t_values = np.linspace(config.z_T/config.c * config.initial_t_zT, config.z_T/config.c * config.final_t_zT, config.N_t)
+    z_values = np.linspace(0, config.N_z*config.delta_z, config.N_z)
+
+    # We store the range of n, kn and gn in arrays of length N_max
+    n_values = np.linspace(0, config.N_max-1, config.N_max, dtype=int)
+    g_n_values = g_n_rect_delta(n_values, config)
+    k_n_values = 2 * np.pi * n_values
+
+
+    # We perform the integrals
+    integrals_array = perform_integrals(config)
+
+
+    # Component 1: - k_n_values[n] * integrals_array[n,t,z]
+    integrals_part = - k_n_values[:,np.newaxis,np.newaxis] * z_values[np.newaxis,np.newaxis,:] * integrals_array
+
+    # Component 2: sin(omega * (t_values[i] - z_values[j])) * Heaviside(t_values[i] - z_values[j])
+    heaviside_part = np.sin(config.omega * (t_values[np.newaxis,:,np.newaxis] - z_values[np.newaxis,np.newaxis,:])) * np.heaviside(t_values[np.newaxis,:,np.newaxis] - z_values[np.newaxis,np.newaxis,:], 0.5)
+
+    # Combine the components
+    coeffs = g_n_values[:,np.newaxis,np.newaxis] * (integrals_part + heaviside_part)
 
     return coeffs
 
 def generate_amplitude_field(config):
+    '''Computes the value of the solution $u(t,x,z)$ for each allowed value of t, x and z for the parameters in config.
+    
+    Parameters
+    ----------
+    config : TalbotConfig
+        The class containing all the parameters of the simulation.
+    
+    Returns
+    -------
+    field : np.ndarray of floats
+        The values of the field $u(t,x,z)$ at each t,x,z. The array is of shape (N_t, N_x, N_z).
+    '''
+
+
+    # We compute the coefficients $c_n (t,z)$
     coeffs = generate_coeffs(config)
-    field = jnp.zeros([config.N_t, config.N_x, config.N_z])
-    k_n = 2 * jnp.pi * jnp.arange(config.N_max) / config.d
+
+    # We compute the wave numbers
+    k_n = 2 * np.pi * np.arange(config.N_max)
+
     # Create the grid for the cosine terms
-    x_grid = jnp.arange(config.N_x) * config.delta_x
-    cos_values = jnp.cos(jnp.outer(k_n, x_grid))
-    del x_grid
-    # Compute the contribution to E from the cosine term. 
-    # For this we expand dimensions of coeffs and cos_values to match the desired broadcast shape.
-    # New shapes:       (N_max, N_t, 1, N_z)        (N_max, 1, N_x,1)
-    field_update = coeffs[:, :, jnp.newaxis, :] * cos_values[:, jnp.newaxis, :, jnp.newaxis]
-    # Sum over n (along axis 0) to update E
-    field += jnp.sum(field_update, axis = 0)  # Shape: (N_t, N_x, N_z)
+    x_grid = np.arange(config.N_x) * config.delta_x
+    cos_values = np.cos(np.outer(k_n, x_grid))
+    del x_grid, k_n
+
+    field = np.zeros([config.N_t, config.N_x, config.N_z])
+
+    # Iterate over chunks of n axis to avoid memory problems
+    chunk_size = 2  # Choose a reasonable chunk size based on available memory
+    for i in tqdm(range(0, coeffs.shape[0], chunk_size)):
+        chunk_coeffs = coeffs[i:i+chunk_size]
+        chunk_cos_values = cos_values[i:i+chunk_size]
+        
+        # Compute the chunk of the contribution to E from the cosine term. 
+        # For this we expand dimensions of coeffs and cos_values to match the desired broadcast shape.
+        # New shapes:       (N_max, N_t, 1, N_z)        (N_max, 1, N_x,1)
+        field_update_chunk = chunk_coeffs[:, :, np.newaxis, :] * chunk_cos_values[:, np.newaxis, :, np.newaxis]
+        
+        # Sum over n (along axis 0) and add to the field
+        field += np.sum(field_update_chunk, axis=0)
+        del field_update_chunk
+    del cos_values, coeffs, chunk_coeffs, chunk_cos_values
+
     return field
 
 def resize_field(field, config):
+    '''Extends the domain of the solution from $0 \leq x \leq d/2$ to $-d \leq x \leq d$ using its symmetry.
+    
+    Parameters
+    ----------
+    field : np.ndarray of floats
+        The values of the field $u(t,x,z)$ at each t,x,z for $0 \leq x \leq d/2$. The array is of shape (N_t, N_x, N_z).
+    config : TalbotConfig
+        The class containing all the parameters of the simulation.
+    
+    Returns
+    -------
+    resized_field : np.ndarray of floats
+        The values of the field $u(t,x,z)$ at each t,x,z with for $-d \leq x \leq d$. Its shape is (N_t, 4 N_x, N_z).
+    '''
+    # We allocate the new array
     resized_field = np.empty([config.N_t, 4 * config.N_x, config.N_z])
+
+    # Define an auxiliary array
     reversed_order = np.array([n for n in range(config.N_x - 1, -1, -1)])
+
     resized_field[:, 0:config.N_x, :] = field[:,0:config.N_x,:]
     resized_field[:,config.N_x:2*config.N_x,:] = field[:,reversed_order, :]
     resized_field[:,2*config.N_x:3*config.N_x,:] = field[:,0:config.N_x,:]
     resized_field[:,3*config.N_x:4*config.N_x,:] = field[:,reversed_order, :]
     return resized_field
 
-def plot_field(t_i, field, config, folder_path, save_field = False):
+def plot_field(field, config, folder_path, title, file_name, save_field = False, difference = False, cmap = 'gray'):
+    '''Plots the field at time t_i and saves the image in folder_path as a PNG.
+
+    Parameters
+    ----------
+    field : np.ndarray of floats
+        Field to be plotted. Must of size (N_t, 4 N_x, N_z).
+    config : TalbotConfig
+        Class storing the parameters of the simulation.
+    folder_path : string
+        Path where the image must be stored. The folder MUST exist.
+    title : string
+        Title of the plot
+    file_name : string
+        Name of the file.
+    save_field : bool, optional
+        Whether the image should be saved as a txt file alongside the PNG image.
+    difference : bool, optional
+        Whether the values of the image run from -A^2 to A^2 (True) or from 0 to A^2 (False).
+    cmap : str, optional
+        Colormap used for the images. See documentation of matplotlib. Some recommended options are gray and turbo.
+
+    Returns
+    -------
+    None
+    '''
     cm = 1/2.54
     plt.figure(figsize=(32*cm, 18*cm))
-    plt.title('Gauge Field at $t = ' + str(round(t_i * config.delta_t/(config.z_T/config.c),4)) + '\\, Z_T/c$ for $\\frac{d}{\\lambda}='+str(config.d/config._lambda)+'$ and $\\frac{w}{\\lambda}=' + str(config.w/config._lambda)+'$', fontsize = 20, y = 1.05)
-    # Plot the Gauge Field
-    # im = plt.imshow(field[t_i], cmap='RdBu_r', vmin=-config.A, vmax=config.A, interpolation='none')
-    im = plt.imshow(field[t_i], cmap = 'gray', vmin = -config.A, vmax = config.A, interpolation = 'none')
+    plt.title(title, fontsize = 20, y = 1.05)
+
+    # Plot the Field
+    # Some nice options are gray and turbo 
+    im = plt.imshow(field, cmap = cmap, vmin = 0, vmax = (config.d/config.w)**2, interpolation = 'none')
+
     # Label the X axis and set the ticks
     plt.ylabel('Grating', fontsize = 18)
     plt.ylim(0, 4 * config.N_x)
     ticks_x = [0, config.N_x-1, 2 * config.N_x - 1, 3 * config.N_x - 1, 4 * config.N_x - 1]
     labels_x = ['$-d$', '$-\\dfrac{d}{2}$', '$0$', '$\\dfrac{d}{2}$', '$d$']
     plt.yticks(ticks_x, labels_x, fontsize = 16)
+    
     # Label the Y axis and set the ticks
     plt.xlabel('Propagation of light ---->', fontsize = 18)
     plt.xlim(0, config.N_z - 1)
     ticks_z = [0, config.N_z/4, config.N_z/2, 3 * config.N_z/4, config.N_z]
     labels_z = ['$0$', '$\\dfrac{1}{4} Z_T$', '$\\dfrac{1}{2} Z_T$', '$\\dfrac{3}{4} Z_T$', '$Z_T$']
     plt.xticks(ticks_z, labels_z, fontsize = 16)
+    
     # Add the colorbar
-    cbar = plt.colorbar(im, ticks=[-config.A, -config.A/2, 0., config.A/2, config.A], fraction = 0.0458 * config.N_z/(4 * config.N_x), pad = 0.04, shrink = 0.9)
-    cbar.set_label(label = 'Amplitude of the field', fontsize = 18)
-    cbar.ax.set_yticklabels(['$-A$', '$-\\dfrac{A}{2}$', '$0$', '$\\dfrac{A}{2}$', '$A$'], fontsize = 16)
+    if difference:
+        cbar = plt.colorbar(im, ticks=[-(config.d/config.w)**2, -(config.d/config.w)**2/2, 0., (config.d/config.w)**2/2, (config.d/config.w)**2], fraction = 0.0458 * config.N_z/(4 * config.N_x), pad = 0.04, shrink = 0.9)
+        cbar.set_label(label = 'Intensity of the field', fontsize = 18)
+        cbar.ax.set_yticklabels(['$-A^2$', '$-\\dfrac{A^2}{2}$', '$0$', '$\\dfrac{A^2}{2}$', '$A^2$'], fontsize = 16)
+    else:
+        cbar = plt.colorbar(im, ticks=[0., (config.d/config.w)**2/4, (config.d/config.w)**2/2, 3*(config.d/config.w)**2/4, (config.d/config.w)**2], fraction = 0.0458 * config.N_z/(4 * config.N_x), pad = 0.04, shrink = 0.9)
+        cbar.set_label(label = 'Intensity of the field', fontsize = 18)
+        cbar.ax.set_yticklabels(['$0$', '$\\dfrac{A^2}{4}$', '$\\dfrac{A^2}{2}$', '$\\dfrac{3A^2}{4}$', '$A^2$'], fontsize = 16)
+    #cbar.set_label(label = 'Amplitude of the field', fontsize = 18)
+    #cbar.ax.set_yticklabels(['$-A$', '$-\\dfrac{A}{2}$', '$0$', '$\\dfrac{A}{2}$', '$A$'], fontsize = 16) 
 
-    file_name = 'd_λ=' + str(config.d/config._lambda) + '_w_λ=' + str(config.w/config._lambda)+'_' + str(t_i) + '_carpet.pdf'
     plt.savefig(os.path.join(folder_path, file_name), bbox_inches = 'tight', dpi = 300)  
     plt.close()
 
     if save_field:
         # Save the field at time t_i to a txt file
-        txt_file_name = 'd_λ=' + str(config.d/config._lambda) + '_w_λ=' + str(config.w/config._lambda)+'_' + str(t_i) + '_carpet.txt'
-        np.savetxt(os.path.join(folder_path, txt_file_name), field[t_i], delimiter=',')
+        np.savetxt(os.path.join(folder_path, file_name), field, delimiter=',')
+
+def video_from_images(images_path, output_name, fps=24):
+    '''Creates the video showcasing the formation of the Talbot effect.
+
+    Parameters
+    ----------
+    images_path : string
+        Path where the images are stored. The images name must be sorted and end with .png to be identified by this function. The video will be stored in this same path.
+    output_name : string
+        Name of the output file.
+    fps : int, optional
+        Framerate of the video
+
+    Returns
+    -------
+    None
+    '''
+
+    if not os.path.exists(os.path.dirname(images_path)): # We make sure that the images_path exists.
+        os.makedirs(os.path.dirname(images_path))
+        
+    command = f'ffmpeg -framerate {fps} -pattern_type glob -i "{images_path}/*.png" -vf "scale=3288:1928" -c:v libx264 -pix_fmt yuv420p "{output_name}"'
+    os.system(command) # We create the video
